0e20ae7779407ab631d83396f7c8755573259914
JoverZhang <joverzh@gmail.com>
2022-04-12 23:35:06 +0800

    Backup

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,17 @@
+#include "Board.hpp"
+#include <iostream>
+
 int main() {
+  Board board;
+
+  BoardTable &table = board.get_table();
+
+  for (size_t i = 0; i < 9; i++) {
+    for (size_t j = 0; j < 10; j++) {
+      print(table[i][j]);
+    }
+    println();
+  }
+
   return 0;
 }
